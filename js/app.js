4b387299725be48e217ec9150c234b5ef43bb0d8
pyb123 <1196585756@qq.com>
2018-12-18 16:07:42 +0800

    全部完成

--- js/app.js
+++ js/app.js
@@ -1,6 +1,63 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	let list = JSON.parse(localStorage.getItem('todoList')) || []
+
+
+	const vm = new Vue({
+		el: '.todoapp',
+		data: {
+			list,
+			todoName: '',
+			clickId: -1
+		},
+		methods: {
+			addTodo(){
+				this.list.unshift({
+					id: +new Date(),
+					name: this.todoName,
+					iscompleted: false
+				})
+				// 清空input框
+				this.todoName = ''
+			},
+			delTodo(id){
+				// console.log(id);
+				let idx = this.list.findIndex(v => v.id === id)
+				this.list.splice(idx,1)
+			},
+			showEdit(id){
+				this.clickId = id
+			},
+			updateTodo(){
+				this.clickId = -1
+			},
+			clearTodo(){
+				this.list = this.list.filter(v => !v.iscompleted)
+			}
+		},
+		computed:{
+			isShowFooter(){
+				return this.list.length > 0
+			},
+			leftCount(){
+				return this.list.filter(v => !v.iscompleted).length
+			},
+			isShowClear(){
+				return this.list.some(v => v.iscompleted)
+			}
+		},
+		watch: {
+			list: {
+				handler(value){
+					localStorage.setItem('todoList',JSON.stringify(value))
+				},
+				deep: true
+			}
+		}
+	})
+
+	
+	window.vm = vm;
 
 })(window);
